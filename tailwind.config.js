ee01f1796140cb0ce3acd25b2f5cba009e502be3
SrPanquesito <luisgdiazreynoso@gmail.com>
2021-12-22 00:56:07 -0800

    :sparkles: Init repo

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ["./dist/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "imex-blue": "#eef5ff"
+      }
+    },
+    fontFamily: {
+      Poppins: ["Poppins, sans-serif"],
+    },
+    container: {
+      center: true,
+      padding: "1rem",
+    },
+  },
+  plugins: [],
+}
